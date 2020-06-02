0f663eaf1d4672c5dab16684952617d54e1344bb
Your Name <you@example.com>
2020-06-02 15:50:40 +0800

    test

--- proj1a/ArrayDeque.java
+++ proj1a/ArrayDeque.java
@@ -46,25 +46,23 @@ public class ArrayDeque<T> {
         front = (front + 1) % array.length;
         T remove = array[front];
         size -= 1;
+        array[front] = null;
 
         if (array.length <= 16 && size / array.length <= 0.25) {
             resize(array.length / 2);
         }
-        front = (array.length - 1) % array.length;
-        rear = size;
         return remove;
     }
 
     public T removeLast() {
-        rear = (rear - 1) % array.length;
+        rear = (rear - 1 + array.length) % array.length;
         T remove = array[rear];
         size -= 1;
+        array[rear] = null;
 
-        if (array.length <= 16 && size / array.length <= 0.25) {
+        if (array.length <= 16 && array.length >= 4 * size) {
             resize(array.length / 2);
         }
-        front = (array.length - 1) % array.length;
-        rear = size;
         return remove;
     }
 
@@ -97,19 +95,7 @@ public class ArrayDeque<T> {
 
     }
 
-    /*
-    public static void main(String[] args) {
-        ArrayDeque<Integer> a = new ArrayDeque<>();
-        a.addFirst(0);
-        a.addFirst(1);
-        a.addFirst(2);
-        a.addLast(3);
-        a.addLast(4);
-        a.addFirst(5);
-        a.size();
-        a.addLast(7);
-        a.addFirst(8);
-        a.addFirst(9);
-    }
-    */
+
+
+
 }
