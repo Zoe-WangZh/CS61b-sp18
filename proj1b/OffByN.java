f8030072e840dc98092cc3f4d2e8ee35fc62bfc5
Your Name <you@example.com>
2020-06-09 17:25:13 +0800

    3

--- proj1b/OffByN.java
+++ proj1b/OffByN.java
@@ -1,13 +1,13 @@
 public class OffByN implements CharacterComparator {
 
-    private int Num;
+    private int N;
 
     public OffByN(int N) {
-        this.Num = N;
+        this.N = N;
     }
 
     @Override
     public boolean equalChars(char x, char y) {
-        return (x - y == Num || y - x == Num);
+        return (x - y == N || y - x == N);
     }
 }
